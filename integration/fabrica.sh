4ce8ef1318d748e2d4a315ef2db57c386ff64e60
prabhnith <prabhjot.nith@gmail.com>
2016-06-29 17:36:54 +0530

    update in fabrica.sh

--- integration/fabrica.sh
+++ integration/fabrica.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-git clone https://github.com/mutohq/Fabrica.git /var/www/Fabrica-new
-rm -r /var/www/Fabrica/
+cd /var/www/Fabrica/
+git pull origin master
 
-mv /var/www/Fabrica-new /var/www/Fabrica
 service nginx restart
+
+
+# git clone https://github.com/mutohq/Fabrica.git /var/www/Fabrica-new
+# rm -r /var/www/Fabrica/
+
+# mv /var/www/Fabrica-new /var/www/Fabrica
